4b2a64ec686af2e1aad2a6fdedac49deb1b60740
Braden Linick <blinick@alumni.nmu.edu>
2020-11-26 21:34:42 +0800

    Coin summary page finished.

---
+++ src/context/watchListContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react'
+
+export const WatchListContext = createContext();
+
+export const WatchListContextProvider = props => {
+  const [watchList, setWatchList] = useState([
+    'bitcoin', 
+    'ethereum', 
+    'ripple', 
+    'tether'
+  ])
+
+  const deleteCoin = (coin) => {
+    setWatchList(watchList.filter(el => {
+      return el !== coin
+    }))
+  }
+
+  return (
+    <WatchListContext.Provider value={{watchList, deleteCoin}}>
+      {props.children}
+    </WatchListContext.Provider>
+  )
+}
